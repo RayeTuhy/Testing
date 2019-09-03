c2096561e17a17476e9de0613140037b9ee6d90a
Raye Tuhy <RL200485@stu.rlas-116.org>
2019-09-03 13:52:17 -0500

    put in code

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
+    
+    
+    var list = ["1", "2", "3", "4"]
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        return list[row]
+    }
+   
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.textBox.text = self.list[row]
+        self.dropDown.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+            
+        }
+    }
 }
 
