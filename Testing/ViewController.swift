321e286cc5b7463865f54f8f59d5eb9a2bf0be3f
Raye Tuhy <RL200485@stu.rlas-116.org>
2019-09-13 11:00:14 -0500

    drop down semi-working

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -11,32 +11,41 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
     @IBOutlet weak var textBox: UITextField!
-    @IBOutlet weak var dropDown: UIPickerView!
     
     
-    var list = ["1", "2", "3", "4"]
+    let list = ["Forgot", "Charge", "Lost", "Broken"]
     
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        
+        textBox.inputView = pickerView
+        
+        
+    }
+    
+     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return list.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        self.view.endEditing(true)
         return list[row]
     }
    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.textBox.text = self.list[row]
-        self.dropDown.isHidden = true
+        self.textBox.text = list[row]
+       
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField == self.textBox {
-            self.dropDown.isHidden = false
+            
             
         }
     }
